cf7c35d2489fbff828f8b05efa7741468ed599d2
avinashsingh170957 <avinashsingh0957@gmail.com>
2025-12-28 22:36:35 +0530

    Input Acess

---
+++ Access_input_P3/index.js
@@ -0,0 +1,6 @@
+let name = document.getElementById("input1");
+name.value = "Avinash";
+let name2 = document.getElementById("input3");
+name2.value = "Ahmedabad";
+let name3 = document.getElementById("input2");
+name3.value = "Singh";
